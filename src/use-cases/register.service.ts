7b40d6e7c73d51a962fc577e2fa2ca7e5951bdcd
Matheus <matheus.mleall@gmail.com>
2024-06-11 18:29:10 -0300

    feat: working with repository pattern

--- src/use-cases/register.service.ts
+++ src/use-cases/register.service.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
 import { hash } from "bcryptjs";
 
 interface RegisterServiceRequest {
@@ -22,7 +23,7 @@ export async function registerService({
 		throw new Error("User email provided already exists.");
 	}
 
-	await prisma.user.create({
-		data: { name, email, password_hash },
-	});
+	const prismaUsersRepository = new PrismaUsersRepository();
+
+	await prismaUsersRepository.create({ name, email, password_hash });
 }
